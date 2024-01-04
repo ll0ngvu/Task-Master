14a0ecae54580cc6269979423502db85ad078db9
Long Vu <u6699146@anu.edu.au>
2024-01-05 00:50:52 +1100

    Changed UI | Add in List Item

--- task-master/app/components/inputBox.jsx
+++ task-master/app/components/inputBox.jsx
@@ -1,8 +1,9 @@
 import styles from '@/app/styles/inputBox.module.css'
+import { be_vietnam_pro } from '@/app/fonts'
 
 export default function InputBox() {
 
-    const placeholderText = 'Add a task';
+    const placeholderText = 'Add a task...';
 
     const handleFous = (e) => {
         e.target.placeholder = '';
@@ -13,14 +14,16 @@ export default function InputBox() {
     }
 
     return (
-        <div className={styles.inputContainer}>
-            <input
-            type='text'
-            placeholder={placeholderText}
-            onFocus={handleFous}
-            onBlur={handleBlur}
-            className={styles.inputBox}
-            />
+        <div className={be_vietnam_pro.className}>
+            <div className={styles.inputContainer}>
+                <input
+                type='text'
+                placeholder={placeholderText}
+                onFocus={handleFous}
+                onBlur={handleBlur}
+                className={styles.inputBox}
+                />
+            </div>
         </div>
     )
 }
--- task-master/app/components/taskItem.jsx
+++ task-master/app/components/taskItem.jsx
@@ -1,9 +1,22 @@
 import styles from '@/app/styles/taskItem.module.css'
+import { be_vietnam_pro } from '@/app/fonts'
 
-export default function TaskItem() {
+export default function TaskItem({taskDescription, onComplete}) {
     return (
         <div className={styles.tasksContainer}> 
-            <h1>laksdf</h1>
+            <div className={styles.tasksBox}>
+                <div className={styles.taskItem}>
+                    <input
+                        type='checkbox'
+                        id={taskDescription}
+                    />
+                    <label htmlFor={taskDescription} className={be_vietnam_pro.className}>This is task 1</label>
+                    {/* <label htmlFor={taskDescription}>{taskDescription}</label> */}
+                    
+                    <button onClick={() => onComplete(taskDescription)} className={styles.taskRemove}></button>
+
+                </div>
+            </div>
         </div>
     )
 }
