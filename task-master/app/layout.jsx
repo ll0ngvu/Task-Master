31fb7984d33fd6478cacad3b4be948ebc77fc105
Long Vu <u6699146@anu.edu.au>
2024-01-08 17:13:04 +1100

    Small changes for mobile devices

--- task-master/app/layout.jsx
+++ task-master/app/layout.jsx
@@ -20,7 +20,7 @@ export default function RootLayout({ children }) {
         {/* Top Bar Colour for browsers in mobile devices */}
         <meta name="theme-color" content="#ffffff" media="(prefers-color-scheme: light)" />
         <meta name="theme-color" content="#000000" media="(prefers-color-scheme: dark)" />
-        
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="./public/logo.png" sizes="any" />
 
       </Head>
